aeaa9da2a98531ce260de693ce1bd2b645a8902e
Ruslan <shafeevruslanlyceum@yandex.ru>
2019-01-30 01:07:04 +0300

    Оптимизировал коллизии, добавил спрайтов для Марио

--- Player.py
+++ Player.py
@@ -7,20 +7,19 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites, players_group)
 
-        self.frames = cut_sheet(load_image('mario_bros.png'), 19, 1)[0]
-        self.left_frames = [pygame.transform.flip(frame, True, False) for frame in self.frames]
-        self.right_frames = self.frames
+        self.MARIO_IMAGES = self.load_images()
+        self.frames = self.MARIO_IMAGES['normal']['small']
+        self.l_frames = [pygame.transform.flip(frame, True, False) for frame in self.frames]
+        self.r_frames = self.frames
 
         self.cur_frame = 0
 
-        self.jumping = False
-        self.max_jumps = 15
-        self.cur_jump = 0
-
         self.image = self.frames[self.cur_frame]
         self.rect = self.image.get_rect()
-
         self.rect = self.rect.move(x, y)
+
+        self.max_jumps = 15
+        self.cur_jump = 0
 
         self.vx = 0
         self.vy = 0
@@ -29,53 +28,69 @@ class Player(pygame.sprite.Sprite):
 
         self.create_sides()
 
+    def load_images(self):
+        images = {}
+        for name, s_surf, b_surf in zip(['normal', 'fire', 'luigi', 'star_1', 'star_2', 'star_3',
+                                         'underground_1', 'underground_2', 'sastle',
+                                         'underwater_1', 'underwater_2'],
+                                        cut_sheet(load_image('Mario.png'), 14, 11),
+                                        cut_sheet(load_image('Big_Mario.png'), 19, 11)):
+            images[name] = {'small': s_surf, 'big': b_surf}
+        return images
+
+
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % 60
         self.vy += self.a * 2
 
         self.vx = max(min(self.vx, self.max_v), -self.max_v)
         self.vy = max(min(self.vy, self.max_v), -self.max_v)
-
         self.rect = self.rect.move(self.vx, self.vy)
 
-        if pygame.sprite.spritecollideany(self.down_side, tiles_group):
+        self.check_collisions()
+        self.sides_group.draw(screen)
+
+    def check_collisions(self):
+        self.update_sides()
+
+        colided_tile = pygame.sprite.spritecollideany(self.left_side, tiles_group)
+        if colided_tile:
+            self.rect.x = colided_tile.rect.right
+            self.vx = max(0, self.vx)
+            self.update_sides()
+
+        colided_tile = pygame.sprite.spritecollideany(self.right_side, tiles_group)
+        if colided_tile:
+            self.rect.right = colided_tile.rect.x
+            self.vx = min(0, self.vx)
+            self.update_sides()
+
+        colided_tile = pygame.sprite.spritecollideany(self.down_side, tiles_group)
+        if colided_tile:
+            self.rect.bottom = colided_tile.rect.y
+            self.vy = min(0, self.vy)
+            self.update_sides()
             self.cur_jump = 0
         else:
             self.image = self.frames[4]
 
-        self.prevent_falling()
-        if pygame.sprite.spritecollide(self.top_side, tiles_group, True):
-            self.jumping = False
-        self.sides_group.draw(screen)
+        colided_tile = pygame.sprite.spritecollideany(self.top_side, tiles_group)
+        if colided_tile:
+            self.cur_jump = self.max_jumps
+            colided_tile.kill()
 
-    def prevent_falling(self):
-        self.update_sides()
-        for side, x, y in [(self.down_side, 0, 1), (self.top_side, 0, -1), (self.left_side, -1, 0),
-                           (self.right_side, 1, 0)]:
-            if pygame.sprite.spritecollideany(side, tiles_group):
-                while pygame.sprite.spritecollideany(side, tiles_group):
-                    self.rect.x -= x
-                    self.rect.y -= y
-                    self.update_sides()
-                self.rect.x += x
-                self.rect.y += y
-                self.update_sides()
-                if x != 0:
-                    self.vx = x // abs(x) * min(x // abs(x) * self.vx, 0)
-                if y != 0:
-                    self.vy = y // abs(y) * min(y // abs(y) * self.vy, 0)
+            self.rect.y = colided_tile.rect.bottom
+            self.vy = max(0, self.vy)
+            self.update_sides()
 
     def jump(self):
         if self.cur_jump < self.max_jumps:
-            self.jumping = True
             self.vy = -self.max_v
             self.cur_jump += 1
-        else:
-            self.jumping = False
 
     def right(self):
-        if self.frames is not self.right_frames:
-            self.frames = self.right_frames
+        if self.frames is not self.r_frames:
+            self.frames = self.r_frames
 
         if self.vx < 0:
             self.image = self.frames[3]
@@ -86,8 +101,8 @@ class Player(pygame.sprite.Sprite):
         self.vx += self.a
 
     def left(self):
-        if self.frames is not self.left_frames:
-            self.frames = self.left_frames
+        if self.frames is not self.l_frames:
+            self.frames = self.l_frames
         if self.vx > 0:
             self.image = self.frames[3]
         elif self.vx:
@@ -108,12 +123,12 @@ class Player(pygame.sprite.Sprite):
                     self.jump()
                     any_key_pressed = True
 
-        if self.jumping and not any_key_pressed:
+        if self.cur_jump and not any_key_pressed:
             if pygame.key.get_pressed()[pygame.K_UP]:
                 self.jump()
                 any_key_pressed = True
             else:
-                self.jumping = False
+                self.cur_jump = self.max_jumps
 
 
         for key, func in [(pygame.K_RIGHT, self.right), (pygame.K_LEFT, self.left)]:
@@ -137,7 +152,7 @@ class Player(pygame.sprite.Sprite):
         # Код ниже рисует зеленые линии вокруг Марио для дебага. Удалю, когда пойму, что коллизии работают корректно
         self.top_side.image = pygame.Surface((self.rect.w - self.max_v * 2, 1))
         self.top_side.image.fill((0, 255, 0))
-        self.down_side.image = pygame.Surface((self.rect.w - self.max_v * 2, 1))
+        self.down_side.image = pygame.Surface((self.rect.w, 1))
         self.down_side.image.fill((0, 255, 0))
         self.left_side.image = pygame.Surface((1, self.rect.h - self.max_v * 2))
         self.left_side.image.fill((0, 255, 0))
@@ -147,8 +162,8 @@ class Player(pygame.sprite.Sprite):
     def update_sides(self):
         self.top_side.rect = pygame.Rect(self.rect.x + self.max_v, self.rect.y - 1,
                                          self.rect.w - self.max_v * 2, 1)
-        self.down_side.rect = pygame.Rect(self.rect.x + self.max_v, self.rect.bottom,
-                                          self.rect.w - self.max_v * 2, 1)
+        self.down_side.rect = pygame.Rect(self.rect.x, self.rect.bottom, self.rect.w, 1)
+
         self.left_side.rect = pygame.Rect(self.rect.x - 1, self.rect.y + self.max_v, 1,
                                           self.rect.h - self.max_v * 2)
         self.right_side.rect = pygame.Rect(self.rect.right, self.rect.y + self.max_v, 1,
